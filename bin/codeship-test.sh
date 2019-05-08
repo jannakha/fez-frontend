ddd0a5e6e16bd60ec85c1aa904507b5d8b13d532
Lea de Groot <l.degroot@library.uq.edu.au>
2019-05-08 10:06:23 +1000

    coverage for test

--- bin/codeship-test.sh
+++ bin/codeship-test.sh
@@ -11,15 +11,15 @@ npm run eslint
 printf "\n--- RUNNING UNIT TESTS ---\n"
 # Not running code coverage check for feature branches.
 # Running in series with `runInBand` to avoid CodeShip VM running out of memory
-if [[ ($CI_BRANCH == "master" || $CI_BRANCH == "staging" || $CI_BRANCH == "production") ]]; then
+# if [[ ($CI_BRANCH == "master" || $CI_BRANCH == "staging" || $CI_BRANCH == "production") ]]; then
     printf "(\"$CI_BRANCH\" build INCLUDES code coverage check)\n"
     printf "\n$ npm run test:unit -- --ci --runInBand\n"
     npm run test:unit -- --ci --runInBand
-else
-    printf "(Build of feature branch \"$CI_BRANCH\" SKIPS code coverage check)\n"
-    printf "\n$ npm run test:unit -- --ci --runInBand --no-coverage\n"
-    npm run test:unit -- --ci --runInBand --no-coverage
-fi
+# else
+#     printf "(Build of feature branch \"$CI_BRANCH\" SKIPS code coverage check)\n"
+#     printf "\n$ npm run test:unit -- --ci --runInBand --no-coverage\n"
+#     npm run test:unit -- --ci --runInBand --no-coverage
+# fi
 
 # Run integration tests
 printf "\n--- RUNNING INTEGRATION TESTS ---\n"
